6749c39ee5b5702a0dfd9e9055e87e6f96896f0c
toonarmycaptain <toonarmycaptain@hotmail.com>
2022-04-29 12:32:27 -0500

    Remove db from webapp; call question-db-service

--- webapp/main.py
+++ webapp/main.py
@@ -1,26 +1,18 @@
 from pathlib import Path
 from random import randint
 
+import httpx
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
-from database import get_db_connection, initiate_database
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
-
-# Initialise database
-DATABASE_PATH = Path('database.db')  # get from env var set by helm later?
-
-
-@app.on_event("startup")
-async def initialise_database(database_path: Path = DATABASE_PATH):
-    initiate_database()
 
 
 @app.get("/")
@@ -52,24 +44,11 @@ def ten(request: Request):  # put application's code here
 async def addition_ten(request: Request):  # put application's code here
     addition_sign = '+'
     equals_sign = '='
-    with get_db_connection() as connection:
-        cursor = connection.cursor()
-        question_cat, = cursor.execute(
-            """
-            SELECT id FROM question_category
-            WHERE name = ?;
-            """, ('addition',)).fetchone()
-        q_id, a, b, *unused_cols = cursor.execute(
-            """
-            SELECT * FROM question 
-            WHERE question_category = ? AND answer <= 10
-            ORDER BY RANDOM() 
-            LIMIT 1;
-            """, (question_cat,)).fetchone()
+    question = httpx.get("http://diophanatic-question-database-service:1742/question/addition").json()
 
     return templates.TemplateResponse("two_integers.html",
                                       {"request": request,
-                                       "a": a, "b": b,
+                                       "a": question['argument_1'], "b": question['argument_2'],
                                        "operator": addition_sign,
                                        "append": equals_sign
                                        })
@@ -79,25 +58,11 @@ async def addition_ten(request: Request):  # put application's code here
 async def subtraction_ten(request: Request):  # put application's code here
     subtraction_sign = '-'
     equals_sign = '='
-    with get_db_connection() as connection:
-        cursor = connection.cursor()
-        question_cat, = cursor.execute(
-            """
-            SELECT id
-            FROM question_category 
-            WHERE name = ?;
-            """, ('subtraction',)).fetchone()
-        q_id, a, b, *unused_cols = cursor.execute(
-            """
-            SELECT * FROM question 
-            WHERE question_category = ? AND argument_1 + argument_2 <= 10
-            ORDER BY RANDOM() 
-            LIMIT 1;
-            """, (question_cat,)).fetchone()
+    question = httpx.get("http://diophanatic-question-database-service:1742/question/subtraction").json()
 
     return templates.TemplateResponse("two_integers.html",
                                       {"request": request,
-                                       "a": a, "b": b,
+                                       "a": question['argument_1'], "b": question['argument_2'],
                                        "operator": subtraction_sign,
                                        "append": equals_sign
                                        })
@@ -107,24 +72,11 @@ async def subtraction_ten(request: Request):  # put application's code here
 async def addition_twenty(request: Request):  # put application's code here
     addition_sign = '+'
     equals_sign = '='
-    with get_db_connection() as connection:
-        cursor = connection.cursor()
-        question_cat, = cursor.execute(
-            """
-            SELECT id FROM question_category
-            WHERE name = ?;
-            """, ('subtraction',)).fetchone()
-        q_id, a, b, *unused_cols = cursor.execute(
-            """
-            SELECT * FROM question 
-            WHERE question_category = ? AND argument_1 + argument_2 <= 20
-            ORDER BY RANDOM() 
-            LIMIT 1;
-            """, (question_cat,)).fetchone()
+    question = httpx.get("http://diophanatic-question-database-service:1742/question/addition_to_twenty").json()
 
     return templates.TemplateResponse("two_integers.html",
                                       {"request": request,
-                                       "a": a, "b": b,
+                                       "a": question['argument_1'], "b": question['argument_2'],
                                        "operator": addition_sign,
                                        "append": equals_sign
                                        })
@@ -134,25 +86,11 @@ async def addition_twenty(request: Request):  # put application's code here
 async def subtraction_twenty(request: Request):  # put application's code here
     subtraction_sign = '-'
     equals_sign = '='
-    with get_db_connection() as connection:
-        cursor = connection.cursor()
-        question_cat, = cursor.execute(
-            """
-            SELECT id 
-            FROM question_category
-            WHERE name = ?;
-            """, ('subtraction',)).fetchone()
-        q_id, a, b, *unused_cols = cursor.execute(
-            """
-            SELECT * FROM question 
-            WHERE question_category = ?
-            ORDER BY RANDOM() 
-            LIMIT 1;
-            """, (question_cat,)).fetchone()
+    question = httpx.get("http://diophanatic-question-database-service:1742/question/subtraction_to_twenty").json()
 
     return templates.TemplateResponse("two_integers.html",
                                       {"request": request,
-                                       "a": a, "b": b,
+                                       "a": question['argument_1'], "b": question['argument_2'],
                                        "operator": subtraction_sign,
                                        "append": equals_sign
                                        })
@@ -162,23 +100,10 @@ async def subtraction_twenty(request: Request):  # put application's code here
 def multiplication(request: Request):  # put application's code here
     multiplication_sign = '×'
     equals_sign = '='
-    with get_db_connection() as connection:
-        cursor = connection.cursor()
-        question_cat, = cursor.execute(
-            """
-            SELECT id FROM question_category
-            WHERE name = ?;
-            """, ('multiplication',)).fetchone()
-        q_id, a, b, *unused_cols = cursor.execute(
-            """
-            SELECT * FROM question 
-            WHERE question_category = ?
-            ORDER BY RANDOM() 
-            LIMIT 1;
-            """, (question_cat,)).fetchone()
+    question = httpx.get("http://diophanatic-question-database-service:1742/question/multiplication").json()
 
-    return templates.TemplateResponse('two_integers.html', {"request": request, "a": a,
-                                                            "b": b,
+    return templates.TemplateResponse('two_integers.html', {"request": request,
+                                                            "a": question['argument_1'], "b": question['argument_2'],
                                                             "operator": multiplication_sign,
                                                             "append": equals_sign
                                                             })
